96742ff4136af0c477778ff8c02057290c3484ff
Miguel <mpantoja637@gmail.com>
2026-04-11 19:02:19 -0600

    Subiendo mi proyecto completo

--- AVANCE SPRINT/conexion.php
+++
@@ -1,7 +0,0 @@
-<?php
-$conn = new mysqli("localhost", "root", "", "growsystem");
-
-if ($conn->connect_error) {
-    die("Error de conexión: " . $conn->connect_error);
-}
-?>
--- src/Codigo web/Indoor Web Desarrollo Actual/conexion.php
+++
@@ -1,7 +0,0 @@
-<?php
-$conn = new mysqli("localhost", "root", "", "indoor");
-
-if ($conn->connect_error) {
-    die("Error de conexión: " . $conn->connect_error);
-}
-?>
